27ec2450768fe46c699a553a0f970a6b06d09eaf
FrankLovegood <alexandrogallegos12@gmail.com>
2025-08-20 17:26:09 -0600

    github and ig links added

---
+++ dev-site/vite.config.ts
@@ -0,0 +1,18 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        // Aquí le decimos a Vite cuáles son todas nuestras páginas HTML.
+        main: resolve(__dirname, 'index.html'),
+        projects: resolve(__dirname, 'projects.html'),
+        about: resolve(__dirname, 'about.html'),
+        portal: resolve(__dirname, 'portal.html'),
+        certificate: resolve(__dirname, 'certificate.html'),
+      },
+    },
+  },
+})
