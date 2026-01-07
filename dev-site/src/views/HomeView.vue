b313f4e0c5f7b7e0e6e883dbfa8ac228e1c4afcf
FrankLovelace <alexandrogallegos12@gmail.com>
2026-01-07 14:18:15 -0600

    nos aseguramos de que los estilos de tailwind se importen

---
+++ dev-site/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main class="bg-gray-900 text-white min-h-screen p-8">
+    <h1 class="text-4xl font-bold">test</h1>
+    <p class="mt-4">franklovelace.dev v2</p>
+  </main>
+</template>
