d0061d4a6ad93cffa3d39c0b5a22b8f90056e7e2
FrankLovelace <alexandrogallegos12@gmail.com>
2026-01-07 15:13:51 -0600

    cada seccion se desliza sobre la anterior

--- dev-site/src/views/HomeView.vue
+++ dev-site/src/views/HomeView.vue
@@ -1,25 +1,42 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
 import FullScreenSection from '@/components/FullScreenSection.vue';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+onMounted(() => {
+  const panels: HTMLElement[] = gsap.utils.toArray(".panel");
+  panels.forEach((panel) => {
+    ScrollTrigger.create({
+      trigger: panel,
+      start: "top top",
+      pin: true,
+      pinSpacing: false
+    });
+  });
+});
 </script>
 
 <template>
   <main>
-    <FullScreenSection class="bg-gray-900">
+    <FullScreenSection class="panel bg-gray-900">
       <h1 class="text-5xl font-bold">Sección 1</h1>
       <p class="mt-4 text-xl">Lorem ipsum</p>
     </FullScreenSection>
 
-    <FullScreenSection class="bg-blue-900">
+    <FullScreenSection class="panel bg-blue-900">
       <h1 class="text-5xl font-bold">Sección 2</h1>
       <p class="mt-4 text-xl">Lorem ipsum</p>
     </FullScreenSection>
 
-    <FullScreenSection class="bg-indigo-900">
+    <FullScreenSection class="panel bg-indigo-900">
       <h1 class="text-5xl font-bold">Sección 3</h1>
       <p class="mt-4 text-xl">Lorem ipsum</p>
     </FullScreenSection>
 
-    <FullScreenSection class="bg-purple-900">
+    <FullScreenSection class="panel bg-purple-900">
       <h1 class="text-5xl font-bold">Sección 4</h1>
       <p class="mt-4 text-xl">Lorem ipsum</p>
     </FullScreenSection>
