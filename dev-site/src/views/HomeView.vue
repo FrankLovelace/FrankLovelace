41292aa17f38139b176b19f0229793f23c148359
FrankLovelace <alexandrogallegos12@gmail.com>
2026-01-07 15:28:35 -0600

    gsap maneja el posicionamiento

--- dev-site/src/views/HomeView.vue
+++ dev-site/src/views/HomeView.vue
@@ -7,38 +7,50 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
 
 onMounted(() => {
-  const panels: HTMLElement[] = gsap.utils.toArray(".panel");
+  const panels = gsap.utils.toArray(".panel") as HTMLElement[];
+
   panels.forEach((panel) => {
     ScrollTrigger.create({
       trigger: panel,
       start: "top top",
       pin: true,
-      pinSpacing: false
+      pinSpacing: false,
+
     });
   });
 });
 </script>
 
 <template>
-  <main>
-    <FullScreenSection class="panel bg-gray-900">
+  <main class="text-white">
+    <FullScreenSection class="panel bg-gray-900 z-0">
       <h1 class="text-5xl font-bold">Sección 1</h1>
       <p class="mt-4 text-xl">Lorem ipsum</p>
     </FullScreenSection>
 
-    <FullScreenSection class="panel bg-blue-900">
+    <FullScreenSection class="panel bg-blue-900 z-10">
       <h1 class="text-5xl font-bold">Sección 2</h1>
       <p class="mt-4 text-xl">Lorem ipsum</p>
     </FullScreenSection>
 
-    <FullScreenSection class="panel bg-indigo-900">
+    <FullScreenSection class="panel bg-indigo-900 z-20">
       <h1 class="text-5xl font-bold">Sección 3</h1>
       <p class="mt-4 text-xl">Lorem ipsum</p>
     </FullScreenSection>
 
-    <FullScreenSection class="panel bg-purple-900">
+    <FullScreenSection class="panel bg-purple-900 z-30">
       <h1 class="text-5xl font-bold">Sección 4</h1>
       <p class="mt-4 text-xl">Lorem ipsum</p>
     </FullScreenSection>
+
+    <div class="h-screen bg-transparent"></div>
   </main>
 </template>
+
+<style scoped>
+.panel {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+}
+</style>
