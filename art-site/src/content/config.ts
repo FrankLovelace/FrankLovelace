7bc01bf16f6ea3a9c91830c29efd66bd4b8ff624
FrankLovelace <alexandrogallegos12@gmail.com>
2026-01-18 19:21:17 -0600

    borrón y cuenta nueva

--- art-site/src/content/config.ts
+++ art-site/src/content/config.ts
@@ -1,16 +1,1 @@
-import { defineCollection, z } from 'astro:content';
-
-const personajes = defineCollection({
-	schema: z.object({
-		nombre: z.string(),
-		arquetipo: z.string(), 
-		casta: z.string(), 
-		colorHex: z.string(), 
-		edad: z.number().optional(),
-		descripcion: z.string(), 
-		avatar: z.string().optional(), 
-        tipo: z.enum(['Principal', 'Secundario', 'Sin Apariciones']).default('Principal'),
-	}),
-});
-
-export const collections = { personajes };
+//solo creando la estructura
